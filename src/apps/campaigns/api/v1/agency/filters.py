bf224108b4e452688b7443575e5ddfa97eee0231
Albina <29810425+AlbinaGiliazova@users.noreply.github.com>
2025-09-19 22:05:53 +0300

    Update filters.py

--- src/apps/campaigns/api/v1/agency/filters.py
+++ src/apps/campaigns/api/v1/agency/filters.py
@@ -1,12 +1,26 @@
 import django_filters
+from datetime import datetime, time
+from django.utils import timezone
 
 from models.models import Campaign
 
 
 class CampaignFilter(django_filters.FilterSet):
-    created_after = django_filters.DateFilter(field_name="created_at", lookup_expr="gte")
-    created_before = django_filters.DateFilter(field_name="created_at", lookup_expr="lte")
+    created_after = django_filters.DateFilter(method='filter_created_after')
+    created_before = django_filters.DateFilter(method='filter_created_before')
+
+    def filter_created_after(self, queryset, name, value):
+        dt = datetime.combine(value, time.min)
+        if timezone.is_naive(dt):
+            dt = timezone.make_aware(dt, timezone.utc)
+        return queryset.filter(created_at__gte=dt)
+
+    def filter_created_before(self, queryset, name, value):
+        dt = datetime.combine(value, time.max)
+        if timezone.is_naive(dt):
+            dt = timezone.make_aware(dt, timezone.utc)
+        return queryset.filter(created_at__lte=dt)
 
     class Meta:
         model = Campaign
-        fields = ["created_after", "created_before"]
+        fields = ['created_after', 'created_before']
