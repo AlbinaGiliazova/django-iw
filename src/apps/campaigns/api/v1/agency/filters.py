b3da398bbf7337b5a11ecc3df621a2f964c4412b
Гилязова Альбина <giliazova@mail.ru>
2025-09-19 22:16:49 +0300

    Tests fixed

--- src/apps/campaigns/api/v1/agency/filters.py
+++ src/apps/campaigns/api/v1/agency/filters.py
@@ -1,26 +1,28 @@
-import django_filters
 from datetime import datetime, time
+
+import django_filters
+import pytz
 from django.utils import timezone
 
 from models.models import Campaign
 
 
 class CampaignFilter(django_filters.FilterSet):
-    created_after = django_filters.DateFilter(method='filter_created_after')
-    created_before = django_filters.DateFilter(method='filter_created_before')
+    created_after = django_filters.DateFilter(method="filter_created_after")
+    created_before = django_filters.DateFilter(method="filter_created_before")
 
-    def filter_created_after(self, queryset, name, value):
+    def filter_created_after(self, queryset, _, value):  # noqa: PLR6301
         dt = datetime.combine(value, time.min)
         if timezone.is_naive(dt):
-            dt = timezone.make_aware(dt, timezone.utc)
+            dt = timezone.make_aware(dt, pytz.utc)
         return queryset.filter(created_at__gte=dt)
 
-    def filter_created_before(self, queryset, name, value):
+    def filter_created_before(self, queryset, _, value):  # noqa: PLR6301
         dt = datetime.combine(value, time.max)
         if timezone.is_naive(dt):
-            dt = timezone.make_aware(dt, timezone.utc)
+            dt = timezone.make_aware(dt, pytz.utc)
         return queryset.filter(created_at__lte=dt)
 
     class Meta:
         model = Campaign
-        fields = ['created_after', 'created_before']
+        fields = ["created_after", "created_before"]
