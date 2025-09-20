e339b7e6f1e62634b5aceca2100fe0d67cf06ce8
Albina <29810425+AlbinaGiliazova@users.noreply.github.com>
2025-09-20 15:48:05 +0300

    Update filters.py

--- src/apps/campaigns/api/v1/agency/filters.py
+++ src/apps/campaigns/api/v1/agency/filters.py
@@ -15,6 +15,7 @@ class CampaignFilter(django_filters.FilterSet):
     created_after = django_filters.DateFilter(method="filter_created_after")
     created_before = django_filters.DateFilter(method="filter_created_before")
     ids = NumberInFilter(field_name="id", lookup_expr="in")
+    search = django_filters.CharFilter(method="filter_search")
 
     def filter_created_after(self, queryset, _, value):  # noqa: PLR6301
         dt = datetime.combine(value, time.min)
@@ -28,6 +29,9 @@ class CampaignFilter(django_filters.FilterSet):
             dt = timezone.make_aware(dt, pytz.utc)
         return queryset.filter(created_at__lte=dt)
 
+     def filter_search(self, queryset, _, value):  # noqa: PLR6301
+        return queryset.search_by_campaign_or_strategy_name(value)
+
     class Meta:
         model = Campaign
-        fields = ["created_after", "created_before"]
+        fields = ["created_after", "created_before", "ids", "search"]
