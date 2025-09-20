277f56e4738d419616897a5d03a48fe745dc347a
Гилязова Альбина <giliazova@mail.ru>
2025-09-20 15:05:21 +0300

    Task 2

--- src/apps/campaigns/api/v1/agency/filters.py
+++ src/apps/campaigns/api/v1/agency/filters.py
@@ -1,15 +1,20 @@
 from datetime import datetime, time
+import pytz
 
 import django_filters
-import pytz
 from django.utils import timezone
 
 from models.models import Campaign
 
 
+class NumberInFilter(django_filters.BaseInFilter, django_filters.NumberFilter):
+    pass
+
+
 class CampaignFilter(django_filters.FilterSet):
     created_after = django_filters.DateFilter(method="filter_created_after")
     created_before = django_filters.DateFilter(method="filter_created_before")
+    ids = NumberInFilter(field_name='id', lookup_expr='in')
 
     def filter_created_after(self, queryset, _, value):  # noqa: PLR6301
         dt = datetime.combine(value, time.min)
